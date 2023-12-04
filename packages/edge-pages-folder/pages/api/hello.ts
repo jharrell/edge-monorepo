9e1a4d2c043e93ee6f583fd0716cd93cc897c276
ruheni <aleruheni@outlook.com>
2023-12-04 16:56:28 +0100

    chore: init edge-pages folder example

---
+++ packages/edge-pages-folder/pages/api/hello.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  data: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ data: 'Hello world' })
+}
+
+export const runtime = 'edge'
